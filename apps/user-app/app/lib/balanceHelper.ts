b25944bf85340f26e4f3f2911d3f8d1c5257039a
Pranay Dwivedi <pranaydwivedi444@gmail.com>
2024-11-08 20:13:12 +0530

    changed packagejson

--- apps/user-app/app/lib/balanceHelper.ts
+++ apps/user-app/app/lib/balanceHelper.ts
@@ -22,7 +22,7 @@ export async function getOnRampTransactions() {
       userId: Number(session?.user?.id),
     },
   });
-  return txns.map((t) => ({
+  return txns.map((t : any) => ({
     time: t.startTime,
     amount: t.amount,
     status: t.status,
